3dbb1a4a1114f516a9cf92bdc57da4b581d2f253
Adam Struharnansky <adam.struharnansky@gmail.com>
2024-03-30 14:19:58 +0100

    Using Enum instead of constants

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pygame
 import sys
-from menu import Menu
+
+from enum import Enum
 from field import Field
 from map import Map
+from menu import Menu
+
+
+class GameState(Enum):
+    MENU = 0
+    LOADING_GAME = 1
+
 
 # Initialize Pygame
 pygame.init()
@@ -23,7 +31,7 @@ background_color = (255, 255, 255) # RGB color for white
 #Tu budu premenne ktore potrebujeme mat v hlavnom cykle
 
 #state_of_game premenna sluzi nato aby sme vedeli v akom bude hry sa nachadzame, 0-zakladne menu
-state_of_game = 0
+state_of_game = GameState.MENU
 buttons = []
 
 #tu bude zadefinovanie classes ak je potrebne
@@ -46,7 +54,7 @@ while True:
     screen.fill(background_color)
 
     #Spravanie ak sme v menu state_of_game = 0
-    if state_of_game == 0:
+    if state_of_game == GameState.MENU:
         menu.create_base_menu()
 
         buttons = menu.buttons
@@ -56,10 +64,10 @@ while True:
 
         #Kliknutie na button load Game a zmenenie stavu
         if response == "Load Game"  or response == "New Game":
-            state_of_game = 1
+            state_of_game = GameState.LOADING_GAME
 
     #Nacitanie mapy
-    if state_of_game == 1:
+    if state_of_game == GameState.LOADING_GAME:
         map.load_map()
 
     # Fill the screen with the background color
