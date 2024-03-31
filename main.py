d4182110dd0d25ddc9c5570c302efc9332562e9a
DarthKirtap <patrikmodrovsky@gmail.com>
2024-03-31 20:27:47 +0200

    Proper detection of movment keys

--- main.py
+++ main.py
@@ -19,8 +19,9 @@ pygame.init()
 # Set the size of the window and create it
 size = width, height = 800, 800
 screen = pygame.display.set_mode(size)
-DEBUG = False
 
+DEBUG_ALL = False
+DEBUG_KEY = True
 # Set the title of the window
 pygame.display.set_caption('Pygame Basic Window')
 
@@ -35,6 +36,7 @@ background_color = (255, 255, 255) # RGB color for white
 #state_of_game premenna sluzi nato aby sme vedeli v akom bude hry sa nachadzame, 0-zakladne menu
 state_of_game = GameState.MENU
 buttons = []
+key_states = {} #left, right, up, down
 
 #tu bude zadefinovanie classes ak je potrebne
 menu = Menu(buttons, screen)
@@ -42,13 +44,13 @@ map = Map(screen)
 
 
 
-
-
 # Main loop
+
 while True:
+
     
     for event in pygame.event.get():
-        if DEBUG:
+        if DEBUG_ALL:
             if event.type != pygame.MOUSEMOTION:
                 print("Event: " + str(event))
         if event.type == pygame.QUIT:
@@ -57,6 +59,7 @@ while True:
 
     buttons = []
     screen.fill(background_color)
+
 
     #Spravanie ak sme v menu state_of_game = 0
     if state_of_game == GameState.MENU:
@@ -80,7 +83,55 @@ while True:
     if state_of_game == GameState.LOADING_GAME:
         map.load_map()
         map.draw()
-        state_of_game == GameState.PLAYING_GAME
+        state_of_game = GameState.PLAYING_GAME
+
+    if state_of_game == GameState.PLAYING_GAME:
+        keys=pygame.key.get_pressed()
+         
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            if key_states[pygame.K_RIGHT] < 1:
+                key_states[pygame.K_RIGHT] = 1
+        else:
+            key_states[pygame.K_RIGHT] = 0
+
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            if key_states[pygame.K_LEFT] < 1:
+                key_states[pygame.K_LEFT] = 1
+        else:
+            key_states[pygame.K_LEFT] = 0
+
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            if key_states[pygame.K_DOWN] < 1:
+                key_states[pygame.K_DOWN] = 1
+        else:
+            key_states[pygame.K_DOWN] = 0  
+            
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            if key_states[pygame.K_UP] < 1:
+                key_states[pygame.K_UP] = 1
+        else:
+            key_states[pygame.K_UP] = 0
+            
+
+        for i in key_states.keys():
+            if (key_states[i] == 1):
+                key_states[i] = 2
+                if DEBUG_KEY:
+                    match i:
+                        case 1073741903:
+                            print("right")
+                        case 1073741904:
+                            print("left")
+                        case 1073741905:
+                            print("down")
+                        case 1073741906:
+                            print("up")
+                        case _:
+                            print("Achievement unlocked: How did we get here?")
+
+        map.draw()
+
+    
 
     # Fill the screen with the background color
     
