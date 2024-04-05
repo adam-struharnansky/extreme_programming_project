0a3aa946fd2347b5e12307815fc98e03da04ab6e
Mikalai Halavachenka <55761731+dedinside1337@users.noreply.github.com>
2024-04-05 13:29:03 +0200

    adding potion and effect

--- effect.py
+++ effect.py
@@ -1,6 +1,36 @@
+from creature import Creature
+from enums import EffectType
+
 
 class Effect:
-    def __init__(self):
-        # todo - vymysliet ake budu druhy, ake maju mat funkcie, ako sa budu aplikovat ich funkcie
-        # ci budu nejake kombinacie robit nieco (ak by sa niekomu velmi chcelo), alebo to bude iba kazdy postupne
-        pass
+    def __init__(
+        self,
+        effect_type: EffectType = None,
+        is_negative: bool = False,
+        effect_level: int = 1,
+        effect_duration: int = 1,
+    ):
+        self._is_negative = is_negative
+        self._effect_level = effect_level
+        self._effect_duration = effect_duration
+        self._effect_type = effect_type
+
+    def tick(self, creature: Creature):
+        self._effect_duration -= 1
+        change = self._effect_level * (-1) if self._is_negative else self._effect_level
+        if self._effect_type == EffectType.HEALTH:
+            creature.change_health(change)
+        elif self._effect_type == EffectType.SPEED:
+            creature.change_speed(change)
+        elif self._effect_type == EffectType.EVASION:
+            creature.change_evasion(change)
+
+    def get_effect_duration(self):
+        return self._effect_duration
+
+    def get_effect_level(self):
+        return self._effect_level
+
+    def get_effect_type(self):
+        return self._effect_type
+
---
+++ potion.py
@@ -0,0 +1,55 @@
+from creature import Creature
+from enums import PotionType, PotionSize, PotionLevel, EffectType
+from item import Item
+from effect import Effect
+
+
+class Potion(Item):
+    def __init__(
+        self,
+        potion_type: PotionType = None,
+        potion_size: PotionSize = None,
+        potion_level: PotionLevel = PotionLevel.USUAL,
+    ):
+        self._potion_type = potion_type
+        self._potion_size = potion_size
+        self._potion_level = potion_level
+
+    def use_potion(self, creature: Creature):
+        effect = None
+
+        if self._potion_type == PotionType.HEAL:
+            effect = Effect(
+                EffectType.HEALTH, False, self._potion_level, self._potion_size
+            )
+        elif self._potion_type == PotionType.HASTE:
+            effect = Effect(
+                EffectType.SPEED, False, self._potion_level, self._potion_size
+            )
+        elif self._potion_type == PotionType.SLOW:
+            effect = Effect(
+                EffectType.SPEED, True, self._potion_level, self._potion_size
+            )
+        elif self._potion_type == PotionType.POISON:
+            effect = Effect(
+                EffectType.HEALTH, True, self._potion_level, self._potion_size
+            )
+        elif self._potion_type == PotionType.EVASION:
+            effect = Effect(
+                EffectType.EVASION, False, self._potion_level, self._potion_size
+            )
+
+        if effect:
+            creature.add_effect(effect)
+
+    def get_potion_type(self):
+        return self._potion_type
+
+    def get_potion_size(self):
+        return self._potion_size
+
+    def get_potion_level(self):
+        return self._potion_level
+
+    def get_potion(self):
+        return [self._potion_type, self._potion_size, self._potion_level]
